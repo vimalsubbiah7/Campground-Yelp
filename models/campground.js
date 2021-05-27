0d8b3e1b20070f34b14735f73be5b380c55e6723
vimalsubbiah <vimalsubbiah7@gmail.com>
2021-05-28 01:52:36 +0530

    Added Cluster Map with links

--- models/campground.js
+++ models/campground.js
@@ -7,6 +7,8 @@ const ImageSchema = new Schema({
     filename: String
 });
 
+const opts = { toJSON: { virtuals:true} };
+
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
@@ -38,6 +40,11 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0,20)}....</p>`
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
